68a8f9367a617195c3ebf93be72a3b1f3010d967
thaianhhao223 <thaianhhao322@gmail.com>
2021-11-22 11:32:34 +0700

    Decorator

---
+++ src/HouseBlend.java
@@ -0,0 +1,12 @@
+
+public class HouseBlend extends Beverage{
+	public HouseBlend() {
+		description = "House Blend Coffee";
+		}
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return .89;
+	}
+
+}
